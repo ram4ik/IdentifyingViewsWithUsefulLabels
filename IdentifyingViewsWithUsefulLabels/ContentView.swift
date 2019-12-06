2df03c863f70bb2a8d3494c242fe8947c41019aa
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-12-06 09:17:28 +0200

    Identifying views with useful labels

--- IdentifyingViewsWithUsefulLabels/ContentView.swift
+++ IdentifyingViewsWithUsefulLabels/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "christmas-4645449_640",
+        "coffee-beans-4668463_640",
+        "mountain-hut-4664186_640",
+        "panda-4418773_640",
+        "snow-4668099_640",
+        "winter-4551699_640"
+    ]
+    
+    let labels = [
+        "Mountains",
+        "Coffe beans",
+        "Snow forest",
+        "Red panda",
+        "Snow city street",
+        "Christmas gifts"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...5)
     var body: some View {
-        Text("Hello World")
+        Image(pictures[selectedPicture])
+        .resizable()
+        .scaledToFit()
+        .accessibility(label: Text(labels[selectedPicture]))
+        .accessibility(addTraits: .isButton)
+        .accessibility(removeTraits: .isImage)
+        .onTapGesture {
+            self.selectedPicture = Int.random(in: 0...5)
+        }
     }
 }
 
